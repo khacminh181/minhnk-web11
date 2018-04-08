e2bcf14a6d558dd1c8db1e42749aab591beb3df3
Minh Nguyen <nguyenkhacminh181@gmail.com>
2018-04-08 17:37:42 +0700

    debounce throttle

--- wikipedia-viewer/index.js
+++ wikipedia-viewer/index.js
@@ -2,53 +2,127 @@ $(document).ready(() => {
     listenToFormSubmitEvent()
 })
 
-const show = async event => {
-    event.preventDefault();
-    const keyword = $('#article-search-form__input').val();
-    $('.article-list').html('<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>');
-    const data = await $.ajax({
+
+
+const listenToFormSubmitEvent = () => {
+    // const formElement = $(".article-search-form");
+    // formElement.on("submit", async event => {
+    //     event.preventDefault();
+    //     search();
+    // })
+
+    // Tao ra 1 function search moi goi toi da 1s 1 lan
+    const throttledSearch = _.debounce(search, 1000)
+
+    const inputElement = $("#article-search-form__input");
+    inputElement.on("input", () => {
+        clearData();
+        throttledSearch();
+    });
+    
+
+};
+
+async function search(){
+    
+    const searchQuery = getUserSearchQuery();
+    const data = await searchWiki(searchQuery);
+
+    // check tu khoa search co igong voi tu khoa hien tai cua user khong
+    if(searchQuery != getUserSearchQuery()) {
+        return // khong chay cai ben duoi nua                   
+    }
+
+    processData(data);
+}
+
+function clearData(){
+    $("div.article-list").empty();
+}
+
+async function searchWiki(query) {
+    return await $.ajax({
         url: 'https://en.wikipedia.org/w/api.php',
-        method: 'get',
-        data: {
+        data: { // cau truc data do sever quy dinh
             action: "query",
             list: "search",
             format: "json",
             srprop: "snippet",
             origin: "*",
-            srsearch: encodeURI(keyword)
-        }
+            srsearch: encodeURI(query) // Thay dau cach = "%20"
+        },
+        //success: processData
     })
-    if(data.query.search.length==0)
-        $('.article-list').append("No Result");
-    let box="";
-    for(let i =0; i <data.query.search.length ; i++){
-        box += `
-        <div> 
-            <a href="https://en.wikipedia.org/?curid=${data.query.search[i].pageid}" target="_blank"
-            class="article-view">
-            <h3 className="article-view__title">${data.query.search[i].title}</h3>
-            <p className="article-view__snippet">${data.query.search[i].snippet}</p>
-            </a>
-        </div>     `;
+    // .then(processData) // thay cho callback success ben tren
+    // .catch(console.log)
+}
+
+function getUserSearchQuery() {
+    const inputElement = $("#article-search-form__input");
+    const searchQuery = inputElement.val();
+    return searchQuery;
+}
+
+function processData(data) {
+    // ham nay duoc goi sau khi du lieu duoc server tra ve
+
+    let articleElements = "";
+
+    //map chuyen tu array article thanh array cac the <a>
+
+    if(!(data.query && data.query.search)) {
+        return
     }
-    if(box!="")
-        $('.article-list').append(box);
+
+    const elementString = data.query.search
+        .map(article => `
+            <a href="https://en.wikipedia.org/?curid=${article.pageid}" target="_blank"class="article-view">
+            <h3 className="article-view__title">${article.title}</h3>
+            <p className="article-view__snippet">${article.snippet}</p> </a>`
+        )
+        .join("") // ghep array string lai thanh 1 string
+
+        $("div.article-list").append(elementString);
+    // for(let i = 0; i < data.query.search.length; i++) {
+    //     const article = data.query.search[i];
+
+    //     const articleElement = `
+    //     <a href="https://en.wikipedia.org/?curid=${article.pageid}" target="_blank"class="article-view">
+    //     <h3 className="article-view__title">${article.title}</h3>
+    //     <p className="article-view__snippet">${article.snippet}</p> </a>`
+
+    //     articleElements += articleElement;
+    // }
 }
 
-const listenToFormSubmitEvent = () => {
-    // const formElement = $(".article-search-form");
-    // formElement.on("submit", async event => {
-    //     event.preventDefault()
 
-    //     /**
-    //      * TODO:
-    //      *  - Lấy từ khoá search của người dùng
-    //      *  - Lấy data từ server wikipedia tương ứng với từ khoá search 
-    //      *  - Từ data trả về, tạo một array DOM hiển thị các bài viết của wikipedia
-    //      *  - Đưa các DOM trong array trên vào trong <div class="article-list"></div>
-    //      */
+function debounce(func, wait) {
+    var timeout;
+  
+    return function() {
+      var context = this,
+          args = arguments;
+  
+      var executeFunction = function() {
+        func.apply(context, args);
+      };
+  
+      clearTimeout(timeout);
+      timeout = setTimeout(executeFunction, wait);
+    };
+  };
 
-    // })
-    const formElement = $(".article-search-form");
-    formElement.on("keyup", show);
-}
+  function throttle(func, time) {
+    var wait = false;
+    
+    return function() {
+      if (!wait) {
+        func.call();
+        wait = true;
+        
+        setTimeout(function() {
+          wait = false;
+        }, time);
+      }
+    }
+  }
